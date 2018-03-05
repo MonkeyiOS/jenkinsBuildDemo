513d994d2db803019dcc73612974e96d477e3abb
杨雄 <xiong.yang@dianrong.com>
2018-03-05 16:01:57 +0800

    添加一个空工程

---
+++ JenkinsDemo/JenkinsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsDemo
+//
+//  Created by 杨雄 on 2018/3/5.
+//  Copyright © 2018年 yxiong.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsDemo/JenkinsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsDemo
+//
+//  Created by 杨雄 on 2018/3/5.
+//  Copyright © 2018年 yxiong.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
