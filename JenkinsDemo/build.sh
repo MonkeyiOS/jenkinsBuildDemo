d935e584fc43bd0b7ec3ef347c59c8eaef8d5393
杨雄 <xiong.yang@dianrong.com>
2018-03-08 10:46:36 +0800

    修改

--- JenkinsDemo/build.sh
+++ JenkinsDemo/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 CODE_SIGN_DEVELOPMENT="iPhone Developer: 爷 Yang (H8VZ9L8Y4N)"
-xcodebuild -project JenkinsDemo.xcodeproj -sdk iphoneos -configuration Debug CODE_SIGN_IDENTITY="${CODE_SIGN_DEVELOPMENT}" SYMROOT='$(PWD)'
+PROFILE_NAME="ebc98fe3-523a-429e-9b6a-c0c4faaba668"
+xcodebuild -project JenkinsDemo.xcodeproj -sdk iphoneos -configuration Debug CODE_SIGN_IDENTITY="${CODE_SIGN_DEVELOPMENT}" PROVISIONING_PROFILE_SPECIFIER="${PROFILE_NAME}" SYMROOT='$(PWD)'
 
